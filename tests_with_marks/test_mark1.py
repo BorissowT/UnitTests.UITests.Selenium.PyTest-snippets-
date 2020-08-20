f926719f41e7425f0379b120281fa36249800d1c
tbs99 <timsregisters@gmail.com>
2020-08-20 23:43:53 +0300

    marks

---
+++ tests_with_marks/test_mark1.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+"""
+to start:
+pytest -s -v -m smoke tests_with_marks\test_mark1.py
+
+"""
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
